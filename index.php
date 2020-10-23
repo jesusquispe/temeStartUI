91caec9a9fc705d83260a2e206a5ef64eb51c189
jesusquispe <jesus_rq_36@hotmail.com>
2020-10-23 16:09:44 -0400

    form startUI

---
+++ index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>Hello View!</title>
+  </head>
+  <body>
+    <div class="container">
+    <br><br>
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h1>Html View StartUI</h1>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">                
+                <div class="row"> 
+                    <div class="col-md-3 text-center">
+                    <a href="StartUI/menu.html" class="btn btn-success">menu view</a>
+                    </div>
+                    <div class="col-md-3 text-center">
+                        <a href="StartUI/form.html" class="btn btn-success">Form view</a>
+                    </div>                    
+                    <div class="col-md-3 text-center">
+                        <a href="StartUI/login.html" class="btn btn-success">login view</a>
+                    </div>
+                    <div class="col-md-3 text-center">
+                        <a href="StartUI/restablecer_password.html" class="btn btn-success">Restablecer la contraseña</a>
+                    </div>                    
+                </div><br><br>
+                <div class="row">
+                    <div class="col-md-3 text-center">
+                    <a href="StartUI/form-person.html" class="btn btn-success">Form Person</a>
+                    </div>
+                    <div class="col-md-3"></div>
+                    <div class="col-md-3"></div>
+                    <div class="col-md-3"></div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <!--<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>-->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+              		
+			
+    </script>
+  </body>
+</html>
